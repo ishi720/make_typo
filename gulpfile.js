ff25c156ee682bb0b376a362e9c7fdb542441101
ishi720 <ishizuka720@gmail.com>
2025-04-20 17:37:22 +0900

    gulpが動作するように修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS  = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
